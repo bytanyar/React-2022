59cb909151159c352112fbe92b02cca3f0e297f7
Tanya Robertson <bytanyar@yahoo.com>
2022-04-25 23:34:53 -0400

    setup pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
+import Layout from './components/Layout';
+
 import './styles/app.css';
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/resume" element={<Resume />} />
+        </Routes>
+      </Layout>
+      </BrowserRouter>
     </div>
   );
 }
