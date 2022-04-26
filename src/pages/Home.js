eb65ae586ef8f9f857e45ca42cce22e914c73a7e
Tanya Robertson <bytanyar@yahoo.com>
2022-04-26 01:43:17 -0400

    knowledge list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
+import knowledgeData from '../inputs/knowledge';
+
 import '../styles/home.css';
 
 function Home() {
+  const listItems = knowledgeData.map((tech) =>
+    <li>{tech}</li>
+  );
   return (
     <div className="container">
       <div className='box'>
         <h2>Objective</h2>
         <p>Seeking a position that will benefit from my strong work ethic and experience in website development as well as grow my abilities and knowledge.</p>
+      </div>
+      <div className='box'>
+      <ul>
+              {listItems}
+              <li>PC &amp; Macintosh Operating Systems</li>
+          </ul>
       </div>
     </div>
   );
