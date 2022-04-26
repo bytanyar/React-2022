c2b006c3f5c971ea1195db48acb99d8893e8a017
Tanya Robertson <bytanyar@yahoo.com>
2022-04-26 01:12:40 -0400

    basic home page start

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ function Home() {
   return (
     <div className="container">
       <div className='box'>
-        Home
+        <h2>Objective</h2>
+        <p>Seeking a position that will benefit from my strong work ethic and experience in website development as well as grow my abilities and knowledge.</p>
       </div>
     </div>
   );
