c1ad915e996a3ea65604d67dee604f60421a6447
RobertsonTanya <101999308+RobertsonTanya@users.noreply.github.com>
2022-08-01 19:29:57 -0400

    styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,7 +37,7 @@ function Home() {
           </ul>
         </div>))}
       </div>
-      <div>
+      <div className='box'>
         <h2>Education</h2>
         <h3>Coding Dojo - Online Bootcamp</h3>
         <p>Black Belt - MERN Stack</p>
